89beb6d8ba6f313c8c9c94bd7ffb3b937721ce6c
itsyellow04 <143130038+yellowww04@users.noreply.github.com>
2024-11-27 03:24:22 +0700

    Add Fragment and ViewModel Login,SignUp,Home,Setting

--- app/src/main/java/com/example/skincure/ui/signup/SignUp.kt
+++ app/src/main/java/com/example/skincure/ui/signup/SignUp.kt
@@ -1,31 +1,50 @@
 package com.example.skincure.ui.signup
 
+
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.skincure.R
+import com.example.skincure.databinding.FragmentSignUpBinding
 
 class SignUp : Fragment() {
 
-    companion object {
-        fun newInstance() = SignUp()
-    }
-
+    private lateinit var binding: FragmentSignUpBinding
     private val viewModel: SignUpViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.registerButton.apply {
+            setOnClickListener {
+                findNavController().navigate(R.id.action_signUp_to_otp)
+            }
+        }
+
+        binding.googleRegisterButton.apply{
+            setOnClickListener {
+//                viewModel.signInWithGoogle(requireActivity())
+                findNavController().navigate(R.id.action_signUp_to_home)
+            }
+        }
+
+        binding.loginButton.apply {
+            text = Html.fromHtml(getString(R.string.have_account), Html.FROM_HTML_MODE_LEGACY)
+            movementMethod = LinkMovementMethod.getInstance()
+            setOnClickListener {
+                findNavController().navigate(R.id.action_signUp_to_login)
+            }
+        }
+        return binding.root
     }
 }
