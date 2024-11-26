7223711556be53660711c0da6b5b98f3356baa11
itsyellow04 <143130038+yellowww04@users.noreply.github.com>
2024-11-26 22:00:19 +0700

    Add Fragment and ViewModel Login,SignUp,Home,Setting

--- app/src/main/java/com/example/skincure/ui/mainboard/MainBoard.kt
+++ app/src/main/java/com/example/skincure/ui/mainboard/MainBoard.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.skincure.R
+import com.example.skincure.databinding.FragmentMainBoardBinding
 
 class MainBoard : Fragment() {
+
+    private lateinit var binding : FragmentMainBoardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main_board, container, false)
+        binding = FragmentMainBoardBinding.inflate(inflater, container, false)
+
+        binding.loginButton.setOnClickListener{
+            findNavController().navigate(R.id.action_mainBoard_to_login)
+        }
+
+        binding.signupButton.setOnClickListener{
+            findNavController().navigate(R.id.action_mainBoard_to_signUp)
+        }
+
+        return binding.root
     }
 
 }
