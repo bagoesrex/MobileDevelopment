b6e554ff638d7a24871cfebd0e6a6abde6ab725f
itsyellow04 <143130038+yellowww04@users.noreply.github.com>
2024-12-12 07:25:10 +0700

    add background fix color dark/light

--- app/src/main/java/com/example/skincure/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/skincure/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -70,6 +71,10 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_home_to_camera)
         }
 
+        binding.apply{
+            bottomNavigationView.itemIconTintList = ContextCompat.getColorStateList(requireContext(), R.color.black)
+            bottomNavigationView.itemTextColor = ContextCompat.getColorStateList(requireContext(), R.color.black)
+        }
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
