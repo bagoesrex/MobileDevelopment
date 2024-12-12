153407ccacc03328e5b9dbab13b2c6de1032515f
bagoesrex <bagoesrex@gmail.com>
2024-12-12 07:20:06 +0700

    feat(dashboard): add getCount function from favViewModel and historyViewModel

--- app/src/main/java/com/example/skincure/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/skincure/ui/favorite/FavoriteViewModel.kt
@@ -12,10 +12,20 @@ class FavoriteViewModel(private val repository: Repository) : ViewModel() {
     private val _favoriteList = MutableLiveData<List<FavoriteResult>>()
     val favoriteList: LiveData<List<FavoriteResult>> = _favoriteList
 
+    private val _favoriteCount = MutableLiveData<Int>()
+    val favoriteCount: LiveData<Int> = _favoriteCount
+
     fun getAllFavorite() {
         viewModelScope.launch {
             val results = repository.getAllResult()
             _favoriteList.postValue(results)
+        }
+    }
+
+    fun getFavoriteCount() {
+        viewModelScope.launch {
+            val count = repository.getFavoriteCount()
+            _favoriteCount.postValue(count)
         }
     }
 }
